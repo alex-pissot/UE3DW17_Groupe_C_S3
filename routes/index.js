6823eb356ae8ece4aae505117b3f2af05aa6b18c
Alex Pissot <ylex@hotmail.fr>
2022-01-05 18:53:41 +0100

    Ajout page 'titlerank'

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'UE 3DW16 - Groupe C' });
 });
 
-// /* GET All titles. */
-// router.get('/all-titles', function(req, res, next) {
-//   res.render('all-titles', { title: 'UE 3DW16 - Groupe C' });
-// });
-
-
 /* GET All title  */
 router.get('/alltitles', function(req, res) {
   var db = req.db;
   var collection = db.get('documents');
   collection.find({},{},function(e,docs){
     res.render('alltitles', {
-      "alltitles" : docs
+      "documents" : docs
+    });
+  });
+});
+
+/* GET Docs rank 1 to 10. */
+router.get('/titlerank', function(req, res) {
+  var db = req.db;
+  var collection = db.get('documents');
+  collection.find({},{},function(e,docs){
+    res.render('titlerank', {
+      "documents" : docs
     });
   });
 });
