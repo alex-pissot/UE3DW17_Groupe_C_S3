bd9d8f32dfbf0f295cdf84d94aa49e296f8e375e
abradi1 <chaima.abradi@etu.unilim.fr>
2022-01-07 18:44:43 +0100

    début_partie4

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'UE 3DW16 - Groupe C' });
+router.get('/', function(req, res) {
+  var db = req.db;
+  var collection = db.get('documents');
+  collection.find({},{},function(e,docs){
+    res.render('index', {
+      "documents" : docs
+    });
+  });
 });
 
 /* GET All title  */
